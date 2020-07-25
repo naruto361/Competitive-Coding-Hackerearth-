83d0c55805a4fbb5983efa2bb2f250691cda2636
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-07-25 21:37:20 +0530

    Create CuttingAMaterial.cpp

---
+++ CuttingAMaterial.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main() 
+{	int t;
+cin>>t;
+while(t--)
+	{
+	ll n;
+	cin >> n;
+	cout<<1+ (n*(n+1))/2;
+	cout<<endl;
+	}
+}
+
